36fbec679a92d65ed52b624431e72a0d6ba0c92e
catalunha <catalunha.mj@gmail.com>
2023-05-18 15:08:55 -0300

    rev

--- test/cli_api_http_proj1_test.dart
+++ test/cli_api_http_proj1_test.dart
@@ -1,8 +1,5 @@
-import 'package:cli_api_http_proj1/cli_api_http_proj1.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
+  test('calculate', () {});
 }
